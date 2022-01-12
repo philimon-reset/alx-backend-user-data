ee7cd91d62009b9827d0e214113a159a130e3588
philimon-reset <philimonderib2019@gmail.com>
2022-01-12 05:15:11 +0300

    add #

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -15,39 +15,3 @@ def filter_datum(
     for i in fields:
         message = re.sub(fr'{i}=.+?{separator}', f'{i}={redaction}{separator}', message)
     return message
-
-
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields: List):
-        """ Initialize the formatter """
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        """ format logrecord """
-        return filter_datum(
-            self.fields,
-            self.REDACTION,
-            logging.Formatter.format(
-                self,
-                record),
-            self.SEPARATOR)
-
-
-def get_logger() -> logging.Logger:
-    """ get logger for csv file """
-    logger = logging.getLogger("user_data")
-    stream = logging.StreamHandler()
-    stream.setFormatter(RedactingFormatter)
-    logging.LoggerAdapter(logging.getLogger('user_data'), PII_FIELDS)
-    logger.addHandler(stream)
-    logger.propagate = False
-    logger.setLevel(logging.INFO)
-    return logger
