ca3d551061128693d7ccfa263e563dd5a76ac3c0
philimon-reset <philimonderib2019@gmail.com>
2022-01-13 11:07:29 +0300

    H #

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -24,10 +24,10 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: List):
+    def __init__(self, fields: List[str]):
         """ Initialize the formatter """
         super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
+        self.fields: List[str] = fields
 
     def format(self, record: logging.LogRecord) -> str:
         """ format logrecord """
