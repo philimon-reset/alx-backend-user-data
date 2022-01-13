2ee503f3ab5ad4ea68129a5e3f47c4f1362fe990
philimon-reset <philimonderib2019@gmail.com>
2022-01-13 11:14:10 +0300

    H #

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -13,7 +13,7 @@ PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:  # nopep8
     """ Filter logging """
     for i in fields:
-        message = re.sub(fr'{i}=.+?{separator}', f' {i}={redaction}{separator}', message)  # nopep8
+        message = re.sub(fr'{i}=.+?{separator}', f'{i}={redaction}{separator}', message)  # nopep8
     return message
 
 
