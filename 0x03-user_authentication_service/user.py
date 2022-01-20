d505d11cf2431a73ce8075107ed3a363255864cd
philimon-reset <philimonderib2019@gmail.com>
2022-01-20 06:28:41 +0300

    checker #

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -8,7 +8,9 @@ Base = declarative_base()
 
 
 class User(Base):
+    """ Base class user """
     __tablename__ = 'users'
+
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
