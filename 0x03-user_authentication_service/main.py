aa65dc5505788ebe8097ea1bda461986a5966d93
philimon-reset <philimonderib2019@gmail.com>
2022-01-20 07:42:08 +0300

    checker #

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -9,16 +9,52 @@ from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
 
 
-my_db = DB()
+def register_user(email: str, password: str) -> None:
+    """ regester user to main """
+    pass
 
-email = 'test@test.com'
-hashed_password = "hashedPwd"
+def log_in_wrong_password(email: str, password: str) -> None:
+    """ log in with wrong_password """
+    pass
 
-user = my_db.add_user(email, hashed_password)
-print(user.id)
+def log_in(email: str, password: str) -> str:
+    """ log in and check """
+    pass
 
-try:
-    my_db.update_user(user.id, hashed_password='NewPwd')
-    print("Password updated")
-except ValueError:
-    print("Error")
+def profile_unlogged() -> None:
+    """ profile user checkup """
+    pass
+
+def profile_logged(session_id: str) -> None:
+    """ profile user logged in """
+    pass
+
+def log_out(session_id: str) -> None:
+    """ log out the user """
+    pass
+
+def reset_password_token(email: str) -> str:
+    """ reset password """
+    pass
+
+def update_password(email: str, reset_token: str, new_password: str) -> None:
+    """ update reset password """
+    pass
+
+
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
+
+
+if __name__ == "__main__":
+
+    # register_user(EMAIL, PASSWD)
+    # log_in_wrong_password(EMAIL, NEW_PASSWD)
+    # profile_unlogged()
+    # session_id = log_in(EMAIL, PASSWD)
+    # profile_logged(session_id)
+    # log_out(session_id)
+    # reset_token = reset_password_token(EMAIL)
+    # update_password(EMAIL, reset_token, NEW_PASSWD)
+    # log_in(EMAIL, NEW_PASSWD)
