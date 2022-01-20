dab4fbe8a23d297a750bb60bd89c5f3f4d1647d1
philimon-reset <philimonderib2019@gmail.com>
2022-01-20 07:30:47 +0300

    checker #

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -80,7 +80,7 @@ class Auth:
         except NoResultFound:
             raise ValueError
 
-    def update_password(self, reset_token_: str, password_: str) -> None:
+    def update_password(self, reset_token: str, password: str) -> None:
         """  implement the Auth.update_password method. """
         try:
             check = self._db.find_user_by(reset_token=reset_token_)
